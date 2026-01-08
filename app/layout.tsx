8f796cd6fb6bf2c40bee13a18006db582c4dba34
AJ-505 <abasiono.mbat@gmail.com>
2026-01-08 18:01:41 +0100

    Fix: Resolve favicon mismatch

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,6 @@ export const metadata: Metadata = {
   ],
   icons: {
     icon: [
-      { url: "/favicon.ico", sizes: "any" },
       { url: "/favicon.png", type: "image/png", sizes: "32x32" },
     ],
     apple: "/apple-touch-icon.png",
